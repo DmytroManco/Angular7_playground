54a77575c6096ae594868900dfa6a7b3046c7363
Dmytro Putrin <moncodp@gmail.com>
2019-01-28 15:42:03 +0200

    [Main Page] simple layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollapseComponent } from './shared/collapse/collapse.component';
+import { MainPageComponent } from './features/main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CollapseComponent
+    CollapseComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
